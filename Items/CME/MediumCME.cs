85a63a4ea974fd884d8e37a20f2a062713d123bd
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-19 16:51:24 -0800

    fix the stack

--- Items/CME/MediumCME.cs
+++ Items/CME/MediumCME.cs
@@ -10,14 +10,14 @@ namespace FactoryMod.Items.CME
         {
             Tooltip.SetDefault("Condensed Monster Energy\nCan be deposited into an Energy Unraveler to be converted into TEX.");
             DisplayName.SetDefault("Medium C.M.E");
-
-            item.maxStack = 9999;
         }
 
         public override void SetDefaults()
         {
             item.rare = ItemRarityID.White;
             item.value = 100;
+
+            item.maxStack = 9999;
         }
     }
 }
