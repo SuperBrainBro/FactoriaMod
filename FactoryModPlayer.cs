3b0e35e8210c3e5ecbb8f65b292ffa94c6d5eab2
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-21 09:51:19 -0800

    Player and Wireless CME Bank progress

--- FactoryModPlayer.cs
+++ FactoryModPlayer.cs
@@ -6,10 +6,28 @@ using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using static Terraria.ModLoader.ModContent;
 
+using FactoryMod.Items.CME;
+
 namespace FactoryMod
 {
     public class FactoryModPlayer : ModPlayer
     {
+        public bool collectCME;
 
+        public override void ResetEffects()
+        {
+            collectCME = false;
+        }
+
+        public override void PostUpdate()
+        {
+            if (base.player.whoAmI == Main.myPlayer)
+            {
+                if (collectCME)
+                {
+                    WirelessCMEBankItem.UpdateCME(player);
+                }
+            }
+        }
     }
 }
