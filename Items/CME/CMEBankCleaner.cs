881c2fcdcf87b7390f214ece310bf7c1fcf0bdcf
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-21 10:31:22 -0800

    cme bank cleaner, and tex bank cleaner items

---
+++ Items/CME/CMEBankCleaner.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace FactoryMod.Items.CME
+{
+    public class CMEBankCleaner : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Clears the C.M.E. bank.");
+            DisplayName.SetDefault("C.M.E. Bank Cleaner");
+        }
+
+        public override void SetDefaults()
+        {
+            item.rare = ItemRarityID.White;
+            item.value = 1000;
+
+            item.maxStack = 9999;
+        }
+    }
+}
---
+++ Items/TEX/TEXBankCleaner.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace FactoryMod.Items.TEX
+{
+    public class TEXBankCleaner : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Clears the T.E.X. bank.");
+            DisplayName.SetDefault("T.E.X Bank Cleaner");
+        }
+
+        public override void SetDefaults()
+        {
+            item.rare = ItemRarityID.White;
+            item.value = 1000;
+
+            item.maxStack = 9999;
+        }
+    }
+}
