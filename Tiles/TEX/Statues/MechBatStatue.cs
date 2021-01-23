edf38bda416c3f4b27bec59f35d68e529b9e94f0
SuperBrainBro <superbrainstormers@gmail.com>
2021-01-22 20:40:53 -0800

    Create the mechanical bat statue, tex consumption, and all the variants, and pre-hardmode / hardmode distinctions.

--- Tiles/TEX/Statues/MechBatStatue.cs
+++ Tiles/TEX/Statues/MechBatStatue.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FactoryMod.Items.CME;
+using FactoryMod;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,8 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 using Terraria.World.Generation;
+
+using static Terraria.ModLoader.ModContent;
 
 namespace FactoryMod.Tiles.TEX.Statues
 {
@@ -21,18 +25,18 @@ namespace FactoryMod.Tiles.TEX.Statues
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
 			TileObjectData.addTile(Type);
 			ModTranslation name = CreateMapEntryName();
-			name.SetDefault("Statue");
-			AddMapEntry(new Color(144, 148, 144), name);
+			name.SetDefault("Mechanical Bat Statue");
+			AddMapEntry(new Color(220, 220, 220), name);
 			dustType = 11;
 			disableSmartCursor = true;
 		}
 
 		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
-			Item.NewItem(i * 16, j * 16, 32, 48, ModContent.ItemType<MechBatStatueItem>());
+			Item.NewItem(i * 16, j * 16, 32, 48, ItemType<MechBatStatueItem>());
 		}
 
-		public override void HitWire(int i, int j) {
-			// Find the coordinates of top left tile square through math
+		public override void HitWire(int i, int j)
+		{
 			int y = j - Main.tile[i, j].frameY / 18;
 			int x = i - Main.tile[i, j].frameX / 18;
 
@@ -43,39 +47,87 @@ namespace FactoryMod.Tiles.TEX.Statues
 			Wiring.SkipWire(x + 1, y + 1);
 			Wiring.SkipWire(x + 1, y + 2);
 
-			// We add 16 to x to spawn right between the 2 tiles. We also want to right on the ground in the y direction.
 			int spawnX = x * 16 + 16;
 			int spawnY = (y + 3) * 16;
 
-			// This example shows both item spawning code and npc spawning code, you can use whichever code suits your mod
-			if (Main.rand.NextFloat() < .95f) // There is a 95% chance for item spawn and a 5% chance for npc spawn
+			int npcIndex = -1;
+			if (Wiring.CheckMech(x, y, 10))
 			{
-				// If you want to make a item spawning statue, see below.
-				if (Wiring.CheckMech(x, y, 60) && Item.MechSpawn(spawnX, spawnY, ItemID.SilverCoin) && Item.MechSpawn(spawnX, spawnY, ItemID.GoldCoin) && Item.MechSpawn(spawnX, spawnY, ItemID.PlatinumCoin)) {
-					int id = ItemID.SilverCoin;
-					if (Main.rand.NextBool(100)) {
-						id++;
-						if (Main.rand.NextBool(100)) {
-							id++;
-						}
-					}
-					Item.NewItem(spawnX, spawnY - 20, 0, 0, id, 1, false, 0, false);
+				// Spawning all the variants.
+				int whichMob = -1;
+                if (Main.hardMode)
+                {
+					whichMob = (int)Main.rand.NextFloat(11);
 				}
+                else
+                {
+					whichMob = (int)Main.rand.NextFloat(5);
+				}
+
+				//Pre Hardmode
+				if (whichMob <= 1)
+                {
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.CaveBat);
+				}
+				if (whichMob <= 2)
+				{
+					//Supposed to be spore bat, only in 1.4
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.CaveBat);
+				}
+				if (whichMob <= 3)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.JungleBat);
+				}
+				if (whichMob <= 4)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.Hellbat);
+				}
+				if (whichMob <= 5)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.IceBat);
+				}
+
+				//Hardmode//
+				if (whichMob <= 6)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.GiantBat);
+				}
+				if (whichMob <= 7)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.IlluminantBat);
+				}
+				if (whichMob <= 8)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.Lavabat);
+				}
+				if (whichMob <= 9)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.Slimer);
+				}
+				if (whichMob <= 10)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.GiantFlyingFox);
+				}
+				if (whichMob <= 11)
+				{
+					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.VampireBat);
+				}
+
+
 			}
-			else {
-				// If you want to make an NPC spawning statue, see below.
-				int npcIndex = -1;
-				// 30 is the time before it can be used again. NPC.MechSpawn checks nearby for other spawns to prevent too many spawns. 3 in immediate vicinity, 6 nearby, 10 in world.
-				if (Wiring.CheckMech(x, y, 30) && NPC.MechSpawn((float)spawnX, (float)spawnY, NPCID.Goldfish)) {
-					npcIndex = NPC.NewNPC(spawnX, spawnY - 12, NPCID.Goldfish);
-				}
-				if (npcIndex >= 0) {
-					Main.npc[npcIndex].value = 0f;
-					Main.npc[npcIndex].npcSlots = 0f;
-					// Prevents Loot if NPCID.Sets.NoEarlymodeLootWhenSpawnedFromStatue and !Main.HardMode or NPCID.Sets.StatueSpawnedDropRarity != -1 and NextFloat() >= NPCID.Sets.StatueSpawnedDropRarity or killed by traps.
-					// Prevents CatchNPC
-					Main.npc[npcIndex].SpawnedFromStatue = true;
-				}
+			if (npcIndex >= 0)
+			{
+				Main.npc[npcIndex].value = 0f;
+				Main.npc[npcIndex].npcSlots = 0f;
+				Main.npc[npcIndex].SpawnedFromStatue = false;
+
+				//
+				// Consume TEX.
+				//
+				GetInstance<FactoryModWorld>().worldTEX -= 5;
+				//
+				// Consume TEX.
+				//
 			}
 		}
 	}
@@ -83,13 +135,25 @@ namespace FactoryMod.Tiles.TEX.Statues
 	public class MechBatStatueItem : ModItem
 	{
 		public override void SetStaticDefaults() {
-			DisplayName.SetDefault("Golden Fish Statue");
+			DisplayName.SetDefault("Mechanical Bat Statue");
+			Tooltip.SetDefault("Can be activated with wire.\nConsumes 5 T.E.X. per activation.\nMonster's regular loot table is not affected.\nSpawns all variants of the monster.");
 		}
 
 		public override void SetDefaults() {
 			item.CloneDefaults(ItemID.ArmorStatue);
-			item.createTile = ModContent.TileType<MechBatStatue>();
+			item.createTile = TileType<MechBatStatue>();
 			item.placeStyle = 0;
 		}
-	}
+
+        public override void AddRecipes()
+        {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BatStatue, 1);
+			recipe.AddIngredient(ItemID.DepthMeter, 1);
+			recipe.AddIngredient(ItemType<SmallCME>(), 25);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+			base.AddRecipes();
+		}
+    }
 }
